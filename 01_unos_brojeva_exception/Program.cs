c4916bd321e8cb19a5bbf296052b0dd27f77094d
Ivo.Jurisic <Ivo.Jurisic@DESKTOP-LLFE3FC>
2020-11-30 19:35:32 +0100

    resolved merge conflicts

---
+++ 01_unos_brojeva_exception/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _01_unos_brojeva_exception
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
